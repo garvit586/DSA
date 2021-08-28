e22a3b7db08941bdc056323d551e48b20f5b916f
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-08-28 23:46:42 +0530

    ABCD BCDE

---
+++ src/javaBasic/CharacterPattern3.java
@@ -0,0 +1,23 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class CharacterPattern3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i =1;
+        while(i<=n){
+            int j=1;
+            char ch = (char)('A'+i-1);
+            while (j<=n){
+                System.out.print(ch);
+                ch++;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
+}
