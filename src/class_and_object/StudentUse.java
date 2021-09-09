f0fd428f6069d2d2dc9c67a6c471b8f579ab6a6b
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-09 11:55:07 +0530

    use the student class

--- src/class_and_object/StudentUse.java
+++ src/class_and_object/StudentUse.java
@@ -7,7 +7,18 @@ public class StudentUse {
         Scanner s = new Scanner(System.in);
         Student s1 = new Student();
         s1.name = "ankush";
+        s1.rollNo = 123;
         Student s2 = new Student();
+        s2.name = "anushka";
+        s2.rollNo = 456;
         Student s3 = new Student();
+        s3.name = "radhe";
+        s3.rollNo = 789;
+        System.out.println(s1.name);
+        System.out.println(s1.rollNo);
+        System.out.println(s2.name);
+        System.out.println(s2.rollNo);
+        System.out.println(s3.name);
+        System.out.println(s3.rollNo);
     }
 }
