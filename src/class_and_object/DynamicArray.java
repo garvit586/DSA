cbb3c9ee30731d11a2f432ec02b3483f7400fbec
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-18 15:27:57 +0530

    create add and size method

--- src/class_and_object/DynamicArray.java
+++ src/class_and_object/DynamicArray.java
@@ -1,4 +1,31 @@
 package class_and_object;
 
 public class DynamicArray {
+    private int data[];
+    private int nextIndex;
+
+    public DynamicArray(){
+        data = new int[5];
+        nextIndex = 0;
+    }
+
+    public int size(){
+        return nextIndex;
+    }
+
+    public void add(int element){
+        if (nextIndex == data.length){
+            restructure();
+        }
+        data[nextIndex] = element;
+        nextIndex++;
+    }
+
+    private void restructure(){
+        int temp[] = data;
+        data = new int[data.length*2];
+        for (int i = 0; i < temp.length; i++) {
+            data[i] = temp[i];
+        }
+    }
 }
