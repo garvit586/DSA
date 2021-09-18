c31f8357b9f32528fccf86816e72211039bf89cb
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-18 15:42:49 +0530

    create  get, set  isEmpty and removeLast method

--- src/class_and_object/DynamicArray.java
+++ src/class_and_object/DynamicArray.java
@@ -4,28 +4,64 @@ public class DynamicArray {
     private int data[];
     private int nextIndex;
 
-    public DynamicArray(){
+    public DynamicArray() {
         data = new int[5];
         nextIndex = 0;
     }
 
-    public int size(){
+    public int size() {
         return nextIndex;
     }
 
-    public void add(int element){
-        if (nextIndex == data.length){
+    public void add(int element) {
+        if (nextIndex == data.length) {
             restructure();
         }
         data[nextIndex] = element;
         nextIndex++;
     }
 
-    private void restructure(){
+    private void restructure() {
         int temp[] = data;
-        data = new int[data.length*2];
+        data = new int[data.length * 2];
         for (int i = 0; i < temp.length; i++) {
             data[i] = temp[i];
         }
+    }
+
+    public void set(int index, int element) {
+        if (index > nextIndex) {
+            return;
+        }
+        if (index < nextIndex) {
+            data[index] = element;
+        } else {
+            add(element);
+        }
+    }
+
+    public int get(int index) {
+        if (index >= nextIndex) {
+            return -1;
+        }
+        return data[index];
+    }
+
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int removeLast() {
+        if (size() == 0) {
+            return -1;
+        }
+        int value = data[nextIndex - 1];
+        data[nextIndex - 1] = 0;
+        nextIndex--;
+        return value;
     }
 }
