c8c32979bc0cffad6909d70995dec2dd048fcb76
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-22 13:37:49 +0530

    create a Car class to use as a child class of vehicle

---
+++ src/inheritance/Car.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Car extends Vehicle{
+    public Car(int maxSpeed) {
+        super(maxSpeed);
+    }
+
+    int numGears;
+
+}
--- src/inheritance/VehicleUse.java
+++ src/inheritance/VehicleUse.java
@@ -5,7 +5,10 @@ public class VehicleUse {
         Vehicle v = new Vehicle(100);
         v.weight = 200;
         v.height = 100;
+        v.color = "pink";
 
         v.print();
+
+
     }
 }
