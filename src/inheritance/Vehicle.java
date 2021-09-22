f66cad2f8dff0390ba91e434e336a5f9962003fa
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-22 13:29:11 +0530

    create a constructor

--- src/inheritance/Vehicle.java
+++ src/inheritance/Vehicle.java
@@ -1,4 +1,14 @@
 package inheritance;
 
 public class Vehicle {
+    int color;
+    int maxSpeed;
+    int height;
+    int weight;
+
+    public Vehicle(int maxSpeed){
+        this.maxSpeed = maxSpeed;
+    }
+
+
 }
