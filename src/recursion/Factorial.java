c7aeadca59dbf9f4e218ca3cf27dcc5c8c1a6b6f
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-25 16:44:11 +0530

    change method name

--- src/recursion/Factorial.java
+++ src/recursion/Factorial.java
@@ -2,16 +2,16 @@ package recursion;
 
 public class Factorial {
 
-    public static int Factorial(int n) {
+    public static int factorial(int n) {
         if (n == 0) {
             return 1;
         }
-        int ans = Factorial(n - 1);
+        int ans = factorial(n - 1);
         return n * ans;
     }
 
     public static void main(String[] args) {
-        int ans = Factorial(5);
+        int ans = factorial(5);
         System.out.println(ans);
     }
 }
