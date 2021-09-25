56cd4267a7c7c05f4f02d73e800ebe78587413d0
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-09-25 17:03:22 +0530

    sum of n natural number

---
+++ src/recursion/SumOfNaturalNumbers.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class SumOfNaturalNumbers {
+
+    public static int sum(int n){
+        if (n==1){
+            return 1;
+        }
+        return n+sum(n-1);
+    }
+
+    public static void main(String[] args) {
+        int ans = sum(5);
+        System.out.println(ans);
+    }
+}
