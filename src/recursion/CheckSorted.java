9610599e54d0ef4bf83b6f0a795624b793a20220
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-10-06 11:47:35 +0530

    create a checkSorted2 method

--- src/recursion/CheckSorted.java
+++ src/recursion/CheckSorted.java
@@ -24,8 +24,31 @@ public class CheckSorted {
         }
     }
 
+    public static boolean checkSorted2(int[] input){
+        if (input.length <= 1){
+            return true;
+        }
+        if (input[0] > input[1]){
+            return false;
+        }
+        int smallInput[] = new int[input.length-1];
+        for (int i = 1; i < input.length; i++) {
+            smallInput[i-1] = input[i];
+        }
+
+        boolean smallAns = checkSorted(smallInput);
+
+        if (smallAns){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         int[] input = {5,1,2,3};
         System.out.println(checkSorted(input));
+        System.out.println(checkSorted2(input));
     }
 }
