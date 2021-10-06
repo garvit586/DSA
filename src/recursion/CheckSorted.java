ec7c55bfae58e9d51468e845e9b7de5588108bef
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-10-06 12:06:28 +0530

    create a chechSortedBetter method

--- src/recursion/CheckSorted.java
+++ src/recursion/CheckSorted.java
@@ -2,53 +2,69 @@ package recursion;
 
 public class CheckSorted {
 
-    public static boolean checkSorted(int input[]){
-        if (input.length <= 1){
+    public static boolean checkSorted(int input[]) {
+        if (input.length <= 1) {
             return true;
         }
 
-        int smallInput[] = new int[input.length-1];
+        int smallInput[] = new int[input.length - 1];
         for (int i = 1; i < input.length; i++) {
-            smallInput[i-1] = input[i];
+            smallInput[i - 1] = input[i];
         }
 
         boolean smallAns = checkSorted(smallInput);
-        if (!smallAns){
+        if (!smallAns) {
             return false;
         }
-        if (input[0] <= input[1]){
+        if (input[0] <= input[1]) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static boolean checkSorted2(int[] input){
-        if (input.length <= 1){
+    public static boolean checkSorted2(int[] input) {
+        if (input.length <= 1) {
             return true;
         }
-        if (input[0] > input[1]){
+        if (input[0] > input[1]) {
             return false;
         }
-        int smallInput[] = new int[input.length-1];
+        int smallInput[] = new int[input.length - 1];
         for (int i = 1; i < input.length; i++) {
-            smallInput[i-1] = input[i];
+            smallInput[i - 1] = input[i];
         }
 
         boolean smallAns = checkSorted(smallInput);
 
-        if (smallAns){
+        if (smallAns) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
+    }
+
+    public static boolean chechSortedBetter(int[] input, int startIndex) {
+        if (startIndex == input.length - 1) {
+            return true;
+        }
+
+        if (input[startIndex] > input[startIndex + 1]) {
+            return false;
+        }
+
+        boolean smallAns = chechSortedBetter(input, startIndex + 1);
+        return smallAns;
     }
 
     public static void main(String[] args) {
-        int[] input = {5,1,2,3};
+        int[] input = {5, 1, 2, 3};
+        int[] input2 = { 1, 2, 3};
         System.out.println(checkSorted(input));
         System.out.println(checkSorted2(input));
+        System.out.println(chechSortedBetter(input,0));
+        System.out.println(checkSorted(input2));
+        System.out.println(checkSorted2(input2));
+        System.out.println(chechSortedBetter(input2,0));
     }
 }
