ad67225061525eb09a6cba4e949d305039f2ae84
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-11-30 09:41:18 +0530

    created a insertFirst method

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -3,5 +3,12 @@ package LinkedList;
 public class SinglyLinkedList {
     private static Node head;
 
-
+    public static void insertFirst(Node newNode){
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
 }
