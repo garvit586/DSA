75f40c5f2fc93af6682c203ce0c780874bbeb841
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-11-30 09:41:49 +0530

    created a insertLast method

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -11,4 +11,16 @@ public class SinglyLinkedList {
         newNode.next = head;
         head = newNode;
     }
+
+    public static  void insertLast(Node newNode){
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
 }
