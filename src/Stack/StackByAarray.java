18647a259a07ebb2c1365321dc2700ccd13914ac
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-12-05 15:12:53 +0530

    Created a pop method for stack

--- src/Stack/StackByAarray.java
+++ src/Stack/StackByAarray.java
@@ -32,6 +32,17 @@ public class StackByAarray {
         else {
             top++;
             arr[top] = data;
+            System.out.println("The value " + data + " is Pushed");
+        }
+    }
+
+    public void pop(){
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+        }
+        else{
+            System.out.println("The poped value is " + arr[top]);
+            top --;
         }
     }
 
@@ -40,17 +51,25 @@ public class StackByAarray {
             System.out.println("Stack is Empty");
         }
         else {
-            System.out.println(arr[top]);
+            System.out.println("The peek value is " + arr[top]);
         }
     }
+
+
 
     public static void main(String[] args) {
         StackByAarray stack = new StackByAarray(5);
         stack.isEmpty();
         stack.isFull();
         stack.push(5);
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(25);
         stack.peek();
         stack.isEmpty();
         stack.isFull();
+        stack.pop();
+        stack.isEmpty();
     }
 }
