1bd43093633a606a25c368073664b08e787b4f1b
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-12-10 13:19:11 +0530

    create constructor

---
+++ src/Queue/QueueByArray.java
@@ -0,0 +1,15 @@
+package Queue;
+
+public class QueueByArray {
+    int[] arr;
+    int front;
+    int rear;
+
+    public QueueByArray(int size){
+        arr = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+
+}
