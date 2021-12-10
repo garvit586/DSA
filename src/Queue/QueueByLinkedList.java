a6676f17a2dd66eb76b748754bbbcf2edec070db
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-12-10 14:44:49 +0530

    created a main method

--- src/Queue/QueueByLinkedList.java
+++ src/Queue/QueueByLinkedList.java
@@ -31,21 +31,45 @@ public class QueueByLinkedList {
             System.out.println("underflow");
         }
         else{
-            System.out.println(front.data);
+            System.out.println("the deleted node is " + front.data);
             front = front.next;
         }
     }
 
     public void print(){
+        Node temp = front;
         if (isEmpty()){
             System.out.println("uderflow");
         }
         else {
-            Node temp = front;
             while (temp != null){
-                System.out.println(temp.data);
+                System.out.print(temp.data + " ");
                 temp = temp.next;
             }
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node newNode = new Node(10);
+        Node newNode2 = new Node(20);
+        Node newNode3 = new Node(30);
+        Node newNode4 = new Node(40);
+        Node newNode5 = new Node(50);
+        Node newNode6 = new Node(60);
+        QueueByLinkedList queue = new QueueByLinkedList();
+        queue.isEmpty();
+        queue.enqueue(newNode);
+        queue.enqueue(newNode2);
+        queue.enqueue(newNode3);
+        queue.enqueue(newNode4);
+        queue.enqueue(newNode5);
+        queue.print();
+        queue.dequeue();
+        queue.dequeue();
+        queue.print();
+        queue.enqueue(newNode6);
+        queue.print();
+
     }
 }
