de2afba945f37638729af4596f695cbc1f40a7e8
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-05 23:22:53 +0530

    modified

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -1,11 +1,10 @@
 package Tree;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TreeNode<T> {
     public T data;
-    public List<TreeNode<T>> child;
+    public ArrayList<TreeNode<T>> child;
 
     public TreeNode(T data){
         this.data = data;
