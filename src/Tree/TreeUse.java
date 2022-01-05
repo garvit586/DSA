58072b673e6a1039fff9e9f9030e126bc153d83d
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-05 23:41:08 +0530

    created a print method to print tree

--- src/Tree/TreeUse.java
+++ src/Tree/TreeUse.java
@@ -1,6 +1,17 @@
 package Tree;
 
 public class TreeUse {
+    public static void print(TreeNode<Integer> root){
+        String s = root.data + ":";
+        for (int i = 0; i < root.child.size(); i++) {
+            s = s + root.child.get(i).data + ",";
+        }
+        System.out.println(s);
+        for (int i = 0; i < root.child.size(); i++) {
+            print(root.child.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         TreeNode<Integer> root = new TreeNode<>(40);
         TreeNode<Integer> node1 = new TreeNode<>(50);
@@ -13,6 +24,7 @@ public class TreeUse {
         root.child.add(node5);
         node2.child.add(node3);
         node2.child.add(node4);
+        print(root);
     }
 
 }
