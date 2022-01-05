048fe8acae40bfe2335ee5b872e2dc27b0ec100a
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-05 23:31:24 +0530

    created main class to use tree

--- src/Tree/TreeUse.java
+++ src/Tree/TreeUse.java
@@ -1,5 +1,18 @@
 package Tree;
 
 public class TreeUse {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(40);
+        TreeNode<Integer> node1 = new TreeNode<>(50);
+        TreeNode<Integer> node2 = new TreeNode<>(60);
+        TreeNode<Integer> node3 = new TreeNode<>(70);
+        TreeNode<Integer> node4 = new TreeNode<>(80);
+        TreeNode<Integer> node5 = new TreeNode<>(90);
+        root.child.add(node1);
+        root.child.add(node2);
+        root.child.add(node5);
+        node2.child.add(node3);
+        node2.child.add(node4);
+    }
 
 }
