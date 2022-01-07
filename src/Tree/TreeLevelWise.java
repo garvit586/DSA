4839d0bbefc26a2d0b74abef205551e3e39b2999
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-07 18:41:31 +0530

    create a takeInput method

--- src/Tree/TreeLevelWise.java
+++ src/Tree/TreeLevelWise.java
@@ -1,4 +1,16 @@
 package Tree;
 
+import Queue.QueueByLinkedList;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+
 public class TreeLevelWise {
+    public static TreeNode<Integer> takeInput(){
+        Scanner s  = new Scanner(System.in);
+        System.out.println("Enter root data");
+        int rootData = s.nextInt();
+
+    }
 }
