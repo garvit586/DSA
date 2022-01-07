4264cc3f464c1eac292381634a9cae3787ea5b1b
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-07 16:25:54 +0530

    create main method to check take input and print method

--- src/Tree/TreeByUserInput.java
+++ src/Tree/TreeByUserInput.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class TreeByUserInput {
 
-    public static TreeNode<Integer> takeInput(){
-        Scanner s  = new Scanner(System.in);
+    public static TreeNode<Integer> takeInput(Scanner s){
         System.out.println("Enter the next Node");
         int n = s.nextInt();
         TreeNode<Integer> root = new TreeNode<>(n);
         System.out.println("Enter the number of children of " + n);
         int childCount = s.nextInt();
         for (int i = 0; i < childCount; i++) {
-            TreeNode<Integer> child = takeInput();
+            TreeNode<Integer> child = takeInput(s);
             root.child.add(child);
         }
         return root;
@@ -21,13 +20,16 @@ public class TreeByUserInput {
     public static void print(TreeNode<Integer> root){
         String s = root.data + ":";
         for (int i = 0; i < root.child.size(); i++) {
-            s = s + root.child.get(i);
+            s = s + root.child.get(i).data + ",";
         }
+        System.out.println(s);
         for (int i = 0; i < root.child.size(); i++) {
             print(root.child.get(i));
         }
     }
     public static void main(String[] args) {
-
+        Scanner s  = new Scanner(System.in);
+        TreeNode<Integer> root = takeInput(s);
+        print(root);
     }
 }
