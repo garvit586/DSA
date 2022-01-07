f2b54449c1f75901138b7059e70dd0104c73936c
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-07 16:04:31 +0530

    create a take Input method

--- src/Tree/TreeByUserInput.java
+++ src/Tree/TreeByUserInput.java
@@ -1,4 +1,23 @@
 package Tree;
 
+import java.util.Scanner;
+
 public class TreeByUserInput {
+
+    public static TreeNode<Integer> takeInput(){
+        Scanner s  = new Scanner(System.in);
+        System.out.println("Enter the next Node");
+        int n = s.nextInt();
+        TreeNode<Integer> root = new TreeNode<>(n);
+        System.out.println("Enter the number of children of " + n);
+        int childCount = s.nextInt();
+        for (int i = 0; i < childCount; i++) {
+            TreeNode<Integer> child = takeInput();
+            root.child.add(child);
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+
+    }
 }
