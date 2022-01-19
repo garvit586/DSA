7771fdc94b8239fba78dcc70e91418653065b379
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-19 14:01:45 +0530

    create a method take input

--- src/BinaryTree/BinaryTreeUse.java
+++ src/BinaryTree/BinaryTreeUse.java
@@ -1,4 +1,17 @@
 package BinaryTree;
 
+import java.util.Scanner;
+
 public class BinaryTreeUse {
+    public static BinaryTreeNode<Integer> takeInput(Scanner s){
+        System.out.println("Enter the root");
+        int n = s.nextInt();
+        if (n == -1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(n);
+        root.left = takeInput(s);
+        root.right = takeInput(s);
+        return root;
+    }
 }
