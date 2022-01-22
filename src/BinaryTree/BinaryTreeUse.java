b26ecea43caeb204a44b2243bbb097a7728dd6ab
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-22 23:32:12 +0530

    create a method buildTreeHelper

--- src/BinaryTree/BinaryTreeUse.java
+++ src/BinaryTree/BinaryTreeUse.java
@@ -160,6 +160,35 @@ public class BinaryTreeUse {
         System.out.print(root.data + " ");
     }
 
+    public static BinaryTreeNode<Integer> buildTreeHelper(int[] in, int[] pre, int inS, int inE, int preS, int preE) {
+        if(inS > inE){
+            return null;
+        }
+        int rootData = pre[preS];
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        int rootIndex = -1;
+        for (int i = inS; i <= inE; i++) {
+            if(in[i] == rootData){
+                rootIndex = i;
+                break;
+            }
+        }
+        if(rootIndex == -1){
+            return null;
+        }
+        int leftInS = inS;
+        int leftInE = rootIndex-1;
+        int leftPreS = preS + 1;
+        int leftPreE = leftInE - leftInS + leftPreS;
+        int rightInS = rootIndex + 1;
+        int rightInE = inE;
+        int rightPreS = leftPreE+1;
+        int rightPreE = preE;
+        root.left = buildTreeHelper(in ,pre, leftInS,leftInE,leftPreS,leftPreE);
+        root.right = buildTreeHelper(in ,pre,rightInS,rightInE,rightPreS,rightPreE);
+        return root;
+    }
+
     public static BinaryTreeNode<Integer> buildTree(int[] in, int[] pre){
         return buildTreeHelper(in,pre,0,in.length-1,0,pre.length-1);
     }
@@ -179,5 +208,11 @@ public class BinaryTreeUse {
         PreOrder(root);
         System.out.println();
         PostOrder(root);
+        int[] in = {4,2,5,1,6,3,7};
+        int[] pre = {1,2,4,5,3,6,7};
+        BinaryTreeNode<Integer> root2 = buildTree(in,pre);
+        System.out.println();
+        System.out.println("==========================");
+        printLevelWise(root2);
     }
 }
