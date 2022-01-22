c11132ffe3132a4eb6147bb7c76d812f492b58d6
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-22 19:11:37 +0530

    create a method heightDiameter to reduce time Complexity

--- src/BinaryTree/BinaryTreeUse.java
+++ src/BinaryTree/BinaryTreeUse.java
@@ -113,6 +113,26 @@ public class BinaryTreeUse {
         return Math.max(option1 , Math.max(option2,option3));
     }
 
+    public static Pair<Integer,Integer> heightDiameter(BinaryTreeNode<Integer> root){
+        if(root == null){
+            Pair<Integer,Integer> output = new Pair<>();
+            output.first = 0;
+            output.second = 0;
+            return output;
+        }
+        Pair<Integer,Integer> lo = heightDiameter(root.left);
+        Pair<Integer,Integer> ro = heightDiameter(root.right);
+        int height = 1 + Math.max(lo.first, ro.first);
+        int option1 = lo.first + ro.first;
+        int option2 = lo.second;
+        int option3 = ro.second;
+        int diameter = Math.max(option1 , Math.max(option2,option3));
+        Pair<Integer,Integer> output = new Pair<>();
+        output.first = height;
+        output.second = diameter;
+        return output;
+    }
+
     public static void main(String[] args) {
 //        Scanner s  = new Scanner(System.in);
         BinaryTreeNode<Integer> root = takeInputLevelWise();
@@ -120,5 +140,8 @@ public class BinaryTreeUse {
         System.out.println(countNodes(root));
         System.out.println(height(root));
         System.out.println(diameter(root));
+        System.out.println("=======================");
+        System.out.println("height : " + heightDiameter(root).first);
+        System.out.println("Diameter : " + heightDiameter(root).second);
     }
 }
