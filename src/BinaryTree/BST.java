390d25605dd58fec722d9c5108e4f66030db6809
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-24 17:25:41 +0530

    farmatted the code

--- src/BinaryTree/BST.java
+++ src/BinaryTree/BST.java
@@ -3,15 +3,15 @@ package BinaryTree;
 import java.util.Scanner;
 
 public class BST {
-    public static BinaryTreeNode<Integer> insert(BinaryTreeNode<Integer> root, int value){
-        if(root == null){
+    public static BinaryTreeNode<Integer> insert(BinaryTreeNode<Integer> root, int value) {
+        if (root == null) {
             root = new BinaryTreeNode<>(value);
             return root;
         }
-        if(value < root.data){
+        if (value < root.data) {
             root.left = insert(root.left, value);
         }
-        if(value > root.data){
+        if (value > root.data) {
             root.right = insert(root.right, value);
         }
         return root;
@@ -33,20 +33,20 @@ public class BST {
         print(root.right);
     }
 
-    public static boolean search(BinaryTreeNode<Integer> root, int value){
-        if(root == null){
+    public static boolean search(BinaryTreeNode<Integer> root, int value) {
+        if (root == null) {
             return false;
         }
-        if(root.data == value){
+        if (root.data == value) {
             return true;
         }
-        if(value < root.data){
+        if (value < root.data) {
             return search(root.left, value);
-        }
-        else {
+        } else {
             return search(root.right, value);
         }
     }
+
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(25);
         insert(root, 10);
@@ -58,7 +58,7 @@ public class BST {
         insert(root, 2);
         System.out.println();
         print(root);
-        System.out.println(search(root,40));
-        System.out.println(search(root,100));
+        System.out.println(search(root, 40));
+        System.out.println(search(root, 100));
     }
 }
