7ba889c774630c9ad186f6255e3b83f3fc2f6e05
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-24 18:32:49 +0530

    create a method elementBetweenk1k2

--- src/BinaryTree/BST.java
+++ src/BinaryTree/BST.java
@@ -47,6 +47,19 @@ public class BST {
         }
     }
 
+    public static void elementsBetweenk1k2O(BinaryTreeNode<Integer> root , int k1, int k2){
+        if (root == null){
+            return;
+        }
+        if(k1 < root.data){
+            elementsBetweenk1k2O(root.left,k1,k2);
+        }
+        if(root.data >= k1 && root.data <= k2){
+            System.out.print(root.data + " ");
+        }
+        elementsBetweenk1k2O(root.right,k1,k2);
+    }
+
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(25);
         insert(root, 10);
