9fa0343fef12d58e248d01d6944aa4bf1db3b8da
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-28 14:56:34 +0530

    reformatted the code

--- src/BinaryTree/BST.java
+++ src/BinaryTree/BST.java
@@ -61,104 +61,102 @@ public class BST {
         elementsBetweenk1k2O(root.right, k1, k2);
     }
 
-    public static int minimum(BinaryTreeNode<Integer> root){
-        if(root == null){
-            return  Integer.MAX_VALUE;
+    public static int minimum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
         }
-        return Math.min(root.data , Math.min(minimum(root.left),minimum(root.right)));
+        return Math.min(root.data, Math.min(minimum(root.left), minimum(root.right)));
     }
 
-    public static int maximum(BinaryTreeNode<Integer> root){
-        if(root == null){
-            return  Integer.MIN_VALUE;
+    public static int maximum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
         }
-        return Math.max(root.data , Math.max(maximum(root.left),maximum(root.right)));
+        return Math.max(root.data, Math.max(maximum(root.left), maximum(root.right)));
     }
 
-    public static boolean isBst(BinaryTreeNode<Integer> root){
-        if(root == null){
+    public static boolean isBst(BinaryTreeNode<Integer> root) {
+        if (root == null) {
             return true;
         }
-        int leftMax= maximum(root.left);
+        int leftMax = maximum(root.left);
         int rightMin = minimum(root.right);
 
-        if(root.data < leftMax || root.data > rightMin){
+        if (root.data < leftMax || root.data > rightMin) {
             return false;
         }
-        if(isBst(root.left) && isBst(root.right)){
+        if (isBst(root.left) && isBst(root.right)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public Pair<Boolean,Pair<Integer,Integer>> isBstBetter(BinaryTreeNode<Integer> root){
-        if(root == null){
-            Pair<Boolean,Pair<Integer,Integer>> output = new Pair<Boolean,Pair<Integer,Integer>>();
+    public Pair<Boolean, Pair<Integer, Integer>> isBstBetter(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            Pair<Boolean, Pair<Integer, Integer>> output = new Pair<Boolean, Pair<Integer, Integer>>();
             output.first = true;
-            output.second = new Pair<Integer,Integer>();
+            output.second = new Pair<Integer, Integer>();
             output.second.first = Integer.MAX_VALUE;
             output.second.second = Integer.MIN_VALUE;
             return output;
         }
-        Pair<Boolean,Pair<Integer,Integer>> leftOutput = isBstBetter(root.left);
-        Pair<Boolean,Pair<Integer,Integer>> rightOutput = isBstBetter(root.right);
-        int min = Math.min(root.data, Math.min(leftOutput.second.first,rightOutput.second.first));
-        int max = Math.max(root.data, Math.max(leftOutput.second.second,rightOutput.second.second));
+        Pair<Boolean, Pair<Integer, Integer>> leftOutput = isBstBetter(root.left);
+        Pair<Boolean, Pair<Integer, Integer>> rightOutput = isBstBetter(root.right);
+        int min = Math.min(root.data, Math.min(leftOutput.second.first, rightOutput.second.first));
+        int max = Math.max(root.data, Math.max(leftOutput.second.second, rightOutput.second.second));
         boolean isBST = (root.data < leftOutput.second.second) && (root.data > rightOutput.second.first)
                 && leftOutput.first && rightOutput.first;
-        Pair<Boolean,Pair<Integer,Integer>> output = new Pair<Boolean,Pair<Integer,Integer>>();
+        Pair<Boolean, Pair<Integer, Integer>> output = new Pair<Boolean, Pair<Integer, Integer>>();
         output.first = isBST;
-        output.second = new Pair<Integer,Integer>();
+        output.second = new Pair<Integer, Integer>();
         output.second.first = min;
         output.second.second = max;
         return output;
     }
 
-    public static boolean isBST3(BinaryTreeNode<Integer> root ,int min ,int max){
-        if(root == null){
+    public static boolean isBST3(BinaryTreeNode<Integer> root, int min, int max) {
+        if (root == null) {
             return true;
         }
-        if(root.data < min || root.data > max){
+        if (root.data < min || root.data > max) {
             return false;
         }
-        boolean isLeftOk = isBST3(root.left , min,root.data-1);
-        boolean isRightOk = isBST3(root.right , root.data,max);
+        boolean isLeftOk = isBST3(root.left, min, root.data - 1);
+        boolean isRightOk = isBST3(root.right, root.data, max);
         return isLeftOk && isRightOk;
     }
 
-    public static BinaryTreeNode<Integer> constructBST(int[] arr, int s , int e){
-        if(s>e){
+    public static BinaryTreeNode<Integer> constructBST(int[] arr, int s, int e) {
+        if (s > e) {
             return null;
         }
-        int mid = (s+e)/2;
+        int mid = (s + e) / 2;
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(arr[mid]);
-        root.left = constructBST(arr,s,mid-1);
-        root.right = constructBST(arr,mid+1,e);
+        root.left = constructBST(arr, s, mid - 1);
+        root.right = constructBST(arr, mid + 1, e);
         return root;
     }
 
-    public static ArrayList<Integer> getRootToNodePath(BinaryTreeNode<Integer> root, int data){
-        if(root == null){
+    public static ArrayList<Integer> getRootToNodePath(BinaryTreeNode<Integer> root, int data) {
+        if (root == null) {
             return null;
         }
-        if(root.data == data){
+        if (root.data == data) {
             ArrayList<Integer> output = new ArrayList<>();
             output.add(root.data);
             return output;
         }
-        ArrayList<Integer> leftOutput = getRootToNodePath(root.left,data);
-        if(leftOutput != null){
+        ArrayList<Integer> leftOutput = getRootToNodePath(root.left, data);
+        if (leftOutput != null) {
             leftOutput.add(root.data);
             return leftOutput;
         }
-        ArrayList<Integer> rightOutput = getRootToNodePath(root.right,data);
-        if(rightOutput != null){
+        ArrayList<Integer> rightOutput = getRootToNodePath(root.right, data);
+        if (rightOutput != null) {
             rightOutput.add(root.data);
             return rightOutput;
-        }
-        else{
+        } else {
             return null;
         }
     }
@@ -177,7 +175,7 @@ public class BST {
         System.out.println(search(root, 40));
         System.out.println(search(root, 100));
         elementsBetweenk1k2O(root, 2, 10);
-        int[] arr = {1,2,3,4,5,6,7};
-        print(constructBST(arr,0,6));
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        print(constructBST(arr, 0, 6));
     }
 }
