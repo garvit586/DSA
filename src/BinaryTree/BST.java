5ff34468d904d4313b5d6abdcbd5facd9cf4a1e8
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-01-28 14:55:53 +0530

    craete a method to find root to node path

--- src/BinaryTree/BST.java
+++ src/BinaryTree/BST.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BST {
@@ -136,6 +137,30 @@ public class BST {
         root.left = constructBST(arr,s,mid-1);
         root.right = constructBST(arr,mid+1,e);
         return root;
+    }
+
+    public static ArrayList<Integer> getRootToNodePath(BinaryTreeNode<Integer> root, int data){
+        if(root == null){
+            return null;
+        }
+        if(root.data == data){
+            ArrayList<Integer> output = new ArrayList<>();
+            output.add(root.data);
+            return output;
+        }
+        ArrayList<Integer> leftOutput = getRootToNodePath(root.left,data);
+        if(leftOutput != null){
+            leftOutput.add(root.data);
+            return leftOutput;
+        }
+        ArrayList<Integer> rightOutput = getRootToNodePath(root.right,data);
+        if(rightOutput != null){
+            rightOutput.add(root.data);
+            return rightOutput;
+        }
+        else{
+            return null;
+        }
     }
 
     public static void main(String[] args) {
