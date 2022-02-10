79998149bea5c573e219294ecd246b0dc44b507f
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 01:03:01 +0530

    create a method rehash

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -56,6 +56,26 @@ public class Map<K, V> {
         return null;
     }
 
+    public void rehash() {
+        System.out.println("Rehashing: buckets" + numBuckets + "size" + size);
+        ArrayList<MapNode<K,V>> temp = buckets;
+        buckets = new ArrayList<>();
+        for (int i = 0; i < 2*numBuckets; i++) {
+            buckets.add(null);
+        }
+        size = 0;
+        numBuckets *= 2;
+        for (int i = 0; i < temp.size(); i++) {
+            MapNode<K,V> head = temp.get(i);
+            while(head != null){
+                K key = head.key;
+                V value = head.value;
+                insert(key,value);
+                head = head.next;
+            }
+        }
+    }
+
     public void insert(K key, V value) {
         int bucketIndex = getBucketIndex(key);
         MapNode<K, V> head = buckets.get(bucketIndex);
