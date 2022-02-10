f5c8cd622e018c2aaeb876367b7aaa382a46bebf
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 00:06:56 +0530

    made some changes in insert function

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -26,6 +26,7 @@ public class Map<K, V> {
         while(head != null){
             if(head.key.equals(key)){
                 head.value = value;
+                return;
             }
             head = head.next;
         }
