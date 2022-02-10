d0accc0cf324c5a3687aa11605ce75f566404669
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 01:17:58 +0530

    write map.removeKey to check removeKey method

--- src/HashMap/MapUse.java
+++ src/HashMap/MapUse.java
@@ -8,6 +8,9 @@ public class MapUse {
             System.out.println("i =" + i + "lf = " + map.loadFactor());
         }
 
+        map.removeKey("abc3");
+        map.removeKey("abc7");
+
         for (int i = 0; i < 20; i++) {
             System.out.println("abc" + i + ":" + map.getValue("abc" + i));
         }
