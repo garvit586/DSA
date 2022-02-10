9cc2398268718b099907bc7e01245beed0120b55
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 00:22:02 +0530

    farmatted the code

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -7,29 +7,29 @@ public class Map<K, V> {
     int size;
     int numBuckets;
 
-    public Map(){
+    public Map() {
         numBuckets = 20;
         buckets = new ArrayList<>();
-        for(int i=0; i< 20;i++){
+        for (int i = 0; i < 20; i++) {
             buckets.add(null);
         }
     }
 
-    private int getBucketIndex(K key){
+    private int getBucketIndex(K key) {
         int hashCode = key.hashCode();
         return hashCode % numBuckets;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public V removeKey(K key){
+    public V removeKey(K key) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K ,V> head = buckets.get(bucketIndex);
-        MapNode<K ,V> prev = null;
-        while(head != null){
-            if(head.key.equals(key)) {
+        MapNode<K, V> head = buckets.get(bucketIndex);
+        MapNode<K, V> prev = null;
+        while (head != null) {
+            if (head.key.equals(key)) {
                 if (prev == null) {
                     buckets.set(bucketIndex, head.next);
                 } else {
@@ -43,11 +43,11 @@ public class Map<K, V> {
         return null;
     }
 
-    public V getValue(K key){
+    public V getValue(K key) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K ,V> head = buckets.get(bucketIndex);
-        while(head != null){
-            if(head.key.equals(key)){
+        MapNode<K, V> head = buckets.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key)) {
                 return head.value;
             }
             head = head.next;
@@ -55,11 +55,11 @@ public class Map<K, V> {
         return null;
     }
 
-    public void insert(K key, V value){
+    public void insert(K key, V value) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K ,V> head = buckets.get(bucketIndex);
-        while(head != null){
-            if(head.key.equals(key)){
+        MapNode<K, V> head = buckets.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key)) {
                 head.value = value;
                 return;
             }
@@ -68,6 +68,6 @@ public class Map<K, V> {
         head = buckets.get(bucketIndex);
         MapNode<K, V> newNode = new MapNode<>(key, value);
         newNode.next = head;
-        buckets.set(bucketIndex,newNode);
+        buckets.set(bucketIndex, newNode);
     }
 }
