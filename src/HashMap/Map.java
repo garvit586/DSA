4212cb21962325e61a31f4ed356692712b0e0b61
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 01:19:13 +0530

    formatted the code of map.java

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -56,25 +56,25 @@ public class Map<K, V> {
         return null;
     }
 
-    public double loadFactor(){
-        return (1.0*size)/numBuckets;
+    public double loadFactor() {
+        return (1.0 * size) / numBuckets;
     }
 
     private void rehash() {
         System.out.println("Rehashing: buckets" + numBuckets + "size" + size);
-        ArrayList<MapNode<K,V>> temp = buckets;
+        ArrayList<MapNode<K, V>> temp = buckets;
         buckets = new ArrayList<>();
-        for (int i = 0; i < 2*numBuckets; i++) {
+        for (int i = 0; i < 2 * numBuckets; i++) {
             buckets.add(null);
         }
         size = 0;
         numBuckets *= 2;
         for (int i = 0; i < temp.size(); i++) {
-            MapNode<K,V> head = temp.get(i);
-            while(head != null){
+            MapNode<K, V> head = temp.get(i);
+            while (head != null) {
                 K key = head.key;
                 V value = head.value;
-                insert(key,value);
+                insert(key, value);
                 head = head.next;
             }
         }
@@ -95,8 +95,8 @@ public class Map<K, V> {
         size++;
         newNode.next = head;
         buckets.set(bucketIndex, newNode);
-        double loadFactor = (1.0*size)/numBuckets;
-        if(loadFactor > 0.7){
+        double loadFactor = (1.0 * size) / numBuckets;
+        if (loadFactor > 0.7) {
             rehash();
         }
     }
