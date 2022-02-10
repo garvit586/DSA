b508f0577ab732c41bd354c913f8f65fabcd989f
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 00:16:22 +0530

    create a method getValue

--- src/HashMap/Map.java
+++ src/HashMap/Map.java
@@ -24,6 +24,18 @@ public class Map<K, V> {
         return size;
     }
 
+    public V getValue(K key){
+        int bucketIndex = getBucketIndex(key);
+        MapNode<K ,V> head = buckets.get(bucketIndex);
+        while(head != null){
+            if(head.key.equals(key)){
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
     public void insert(K key, V value){
         int bucketIndex = getBucketIndex(key);
         MapNode<K ,V> head = buckets.get(bucketIndex);
