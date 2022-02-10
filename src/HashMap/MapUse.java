a000769a38e213f90b1c49426833c72f65e3401d
Lenovo <garvit.gupta_cs19@gla.ac.in>
2022-02-11 01:13:03 +0530

    create main method to chech rehashing

--- src/HashMap/MapUse.java
+++ src/HashMap/MapUse.java
@@ -1,4 +1,11 @@
 package HashMap;
 
 public class MapUse {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new Map<>();
+        for (int i = 0; i < 20; i++) {
+            map.insert("abc" + i, i+1);
+            System.out.println("i =" + i + "lf = " + map.loadFactor());
+        }
+    }
 }
